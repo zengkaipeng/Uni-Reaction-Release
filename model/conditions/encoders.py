c8adfa4c0b1c4634567b4d787d6d3b3bc2fde9d4
Xianbin-Liu <liuxb25@gmail.com>
2025-06-28 10:59:56 +0000

    add sm; fix temp(partial)

--- model/conditions/encoders.py
+++ model/conditions/encoders.py
@@ -3,6 +3,7 @@ import torch
 from ..utils import graph2batch
 from .pretrain_gnns import PretrainGIN
 from .SimpleGAT import SimpleCondGAT
+from functools import partial
 
 
 class NumEmbedding(torch.nn.Module):
@@ -31,7 +32,7 @@ class NumEmbeddingWithNan(NumEmbedding):
     def forward(self, num_x):
         empty_mask, bs = torch.isnan(num_x), num_x.shape[0]
         out_result = torch.zeros((bs, self.n_dim)).to(num_x)
-        out_result[~empty_mask] = self.net(num_x[~empty_mask])
+        out_result[~empty_mask] = self.net(num_x[~empty_mask][:, None])
         out_result[empty_mask] = self.nan_embedding
         return out_result
 
@@ -344,3 +345,100 @@ def build_dm_condition_encoder(config, dropout):
         raise NotImplementedError(f'Invalid gnn type {config["type"]}')
 
     return encoder
+
+
+class SMConditionEncoder(torch.nn.Module):
+    def __init__(self, gnn_dim, gnn, mode='mix-all'):
+        super(SMConditionEncoder, self).__init__()
+        self.gnn, self.mode = gnn, mode
+        self.empty_mol = torch.nn.Parameter(torch.randn(gnn_dim))
+        assert mode in ['mix-all', 'mix-catalyst-ligand', 'independent'],\
+            "Invalid condition output mode"
+
+    def forward(self, shared_gnn):
+        node_feat = self.gnn(shared_gnn)
+        node_feat = graph2batch(node_feat, shared_gnn.batch_mask)
+        key_list = ['ligand_smiles', 'solvent_smiles', 'catalyst_smiles']
+        answer = {
+            key: {
+                'embedding': node_feat[idx::4],
+                'meaningful_mask': shared_gnn.batch_mask[idx::4]
+            } for idx, key in enumerate(key_list)
+        }
+
+        if self.mode == 'mix-catalyst-ligand':
+            answer['catalyst and ligand'] = {
+                'embedding': torch.cat([
+                    answer['catalyst']['embedding'],
+                    answer['ligand_smiles']['embedding']
+                ], dim=1),
+                'meaningful_mask': torch.cat([
+                    answer['ligand_smiles']['meaningful_mask'],
+                    answer['ligand_smiles']['meaningful_mask']
+                ], dim=1)
+            }
+            del answer['ligand_smiles']
+            del answer['ligand_smiles']
+        elif self.mode == 'mix-all':
+            all_emb = [answer[k]['embedding'] for k in key_list]
+            all_mask = [answer[k]['meaningful_mask'] for k in key_list]
+            answer = {
+                'mixed': {
+                    'embedding': torch.cat(all_emb, dim=1),
+                    'meaningful_mask': torch.cat(all_mask, dim=1)
+                }
+            }
+
+        for k, v in answer.items():
+            this_empty = ~torch.any(v['meaningful_mask'], dim=1)
+            if torch.any(this_empty).item():
+                v['meaningful_mask'] = torch.cat([
+                    v['meaningful_mask'], this_empty.reshape(-1, 1)
+                ], dim=1)
+                v['embedding'] = torch.cat([
+                    v['embedding'],
+                    self.empty_mol.repeat(this_empty.shape[0], 1, 1)
+                ], dim=1)
+            v['padding_mask'] = torch.logical_not(v['meaningful_mask'])
+
+        return answer
+
+
+def build_basic_condition_encoder(config, dropout, EncoderClass):
+    if config['type'] == 'pretrain':
+        dropout = config['arch'].get('drop_ratio', dropout)
+        config['arch']['drop_ratio'] = dropout
+        if config.get('pretrain_ckpt', '') != '':
+            gnn = PretrainGIN(num_layer=5, emb_dim=300, drop_ratio=dropout)
+            gnn.load_from_pretrained(config['pretrain_ckpt'])
+            freeze_mode = config.get('freeze_mode', 'none')
+            if freeze_mode.startswith('freeze'):
+                freeze_layer = int(freeze_mode.split('-')[1])
+                assert freeze_layer < 5, \
+                    "last layer norm changed, finetune required"
+                gnn.requires_grad_(False)
+                for x in range(freeze_layer):
+                    gnn.batch_norms[x].eval()
+
+                for x in range(freeze_layer, 5):
+                    gnn.batch_norms[x].requires_grad_(True)
+                    gnn.gnns[x].requires_grad_(True)
+            else:
+                assert freeze_mode == 'none', \
+                    f"Invalid freeze mode {freeze_mode}"
+            encoder = EncoderClass(300, gnn, config['mode'])
+        else:
+            gnn = PretrainGIN(**config['arch'])
+            encoder = EncoderClass(config['dim'], gnn, config['mode'])
+    elif config['type'] == 'gat':
+        dropout = config['arch'].get('dropout', dropout)
+        config['arch']['dropout'] = dropout
+        gnn = SimpleCondGAT(**config['arch'])
+        encoder = EncoderClass(config['dim'], gnn, config['mode'])
+    else:
+        raise NotImplementedError(f'Invalid gnn type {config["type"]}')
+
+    return encoder
+
+build_sm_condition_encoder = partial(build_basic_condition_encoder, EncoderClass=SMConditionEncoder)
+# build_cn_condition_encoder = partial(build_basic_condition_encoder, EncoderClass=CNConditionEncoder)
